9058cd53efa87d8b598583bc92599b2b526358ff
Eleni Lixourioti <contact@eleni.co>
2015-03-31 14:57:37 +0100

    Replica set support.

--- run_mongo.sh
+++ run_mongo.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-mongod --fork --logpath=/dev/null --port=27018 --dbpath=mongoram
+mongod --fork --logpath=/tmp/mongolog --port=27018 --dbpath=mongoram --replSet "rs0"
+# You also need to run rs.initiate()
+# If you run into trouble make sure your hostname is in the hosts file and points to 127.0.0.1
